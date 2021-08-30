2039a2b6d22f9f37919a411e3ceca7be99fe569d
nikita0607 <37401771+nikita0607@users.noreply.github.com>
2021-08-30 22:38:50 +0800

    Second change

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame 
+import random 
+
+FPS = 60
+WIDTH = 500
+HEIGHT = 100
+
+#Настройка цвета
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+BLUE = (0,0,255)
+ORANGE = (255, 150, 100)
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+screen.fill(WHITE)
+running = True
+while running:
+    events = pygame.event.get()
+    for i in events:
+        if i.type == pygame.QUIT:
+            running = False
+        if i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_LEFT:
+                moving = 'LEFT'
+            if i.key == pygame.K_RIGHT:
+                moving = 'RIGHT'
+            if i.key == pygame.K_UP:
+                moving = 'UP'
+            if i.key == pygame.K_DOWN:
+                moving = 'DOWN'
+
+
+    pygame.display.update()
+    clock.tick(FPS)
+pygame.quit()
